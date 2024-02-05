7cc5396595ec4b519244d76a71e086d8226cd2ee
keerthi <ksrm1010@gmail.com>
2024-02-05 01:45:40 -0800

    osdfosf

---
+++ src/some/Main.java
@@ -0,0 +1,11 @@
+package some;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello");
+	}
+
+}
